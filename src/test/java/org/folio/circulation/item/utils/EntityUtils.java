30bb377e5e9d6285a5839c5fa96e8f3171998b38
MagzhanArtykov <magzhan_artykov@epam.com>
2023-12-04 19:03:14 +0600

    MODCITEM-17 Add 5 digit lending library code

--- src/test/java/org/folio/circulation/item/utils/EntityUtils.java
+++ src/test/java/org/folio/circulation/item/utils/EntityUtils.java
@@ -34,6 +34,7 @@ public class EntityUtils {
             .instanceTitle("instanceTitle_TEST_UPD")
             .barcode("itemBarcode_TEST_UPD")
             .pickupLocation("pickupLocation_TEST_UPD")
+            .lendingLibraryCode("123456")
             .build();
   }
 
