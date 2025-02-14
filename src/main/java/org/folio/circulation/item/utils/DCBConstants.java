2d739b902f0f88e38165a0d703177ba75eb2edc5
gurleenkaurbp <gurleen.kaur2505@gmail.com>
2025-02-14 14:20:47 +0530

    investigate

--- src/main/java/org/folio/circulation/item/utils/DCBConstants.java
+++ src/main/java/org/folio/circulation/item/utils/DCBConstants.java
@@ -4,6 +4,6 @@ public class DCBConstants {
 
   private DCBConstants() {}
 
-  public static final String LOCATION_ID = "9d1b77e8-f02e-4b7f-b296-3f2042ddac54";
+  public static final String LOCATION_ID = "bdafb452-8e86-4417-84e6-721984bcc968";
 
 }
