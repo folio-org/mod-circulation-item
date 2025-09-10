877f525af4535afbed9afc7561676ca152cf19d0
kapil-epam <kapil_verma1@epam.com>
2025-09-10 17:09:17 +0530

    MODCITEM-44: removed unnecessary method from ResultList class

--- src/main/java/org/folio/circulation/item/model/ResultList.java
+++ src/main/java/org/folio/circulation/item/model/ResultList.java
@@ -1,9 +1,7 @@
 package org.folio.circulation.item.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -26,36 +24,4 @@ public class ResultList<T> {
    * Paged result data.
    */
   private List<T> result = Collections.emptyList();
-
-  /**
-   * Creates empty result list.
-   *
-   * @param <R> generic type for result item.
-   * @return empty result list.
-   */
-  public static <R> ResultList<R> empty() {
-    return new ResultList<>();
-  }
-
-  /**
-   * Creates result list from given resource.
-   *
-   * @param <R> generic type for result item.
-   * @return empty result list.
-   */
-  public static <R> ResultList<R> asSinglePage(List<R> result) {
-    return new ResultList<>(result.size(), result);
-  }
-
-  @SafeVarargs
-  public static <R> ResultList<R> asSinglePage(R... records) {
-    return new ResultList<>(records.length, Arrays.asList(records));
-  }
-
-  // The `key` is required per contract
-  @SuppressWarnings("unused")
-  @JsonAnySetter
-  public void set(String key, List<T> result) {
-    this.result = result;
-  }
 }
